bb27278b3529252aea7b3758c62763839b4e1111
Jamal <khamzaew.j@gmail.com>
2024-10-01 22:03:45 +0300

    sprint_3

--- TIC_TAC_TOE/game.py
+++ TIC_TAC_TOE/game.py
@@ -4,36 +4,45 @@ from gameparts.exceptions import FieldIndexError
 
 def main():
     game = Board()
+    current_player = 'X'
+    running = True
     game.display()
 
-    while True:
-        try:
-            row = int(input('Введите номер строки: '))
-            if row < 0 or row >= game.field_size:
-                raise FieldIndexError
-            column = int(input('Введите номер столбца: '))
-            if column < 0 or column >= game.field_size:
-                raise FieldIndexError
-        except FieldIndexError:
-            print(
-                'Значение должно быть неотрицательным и меньше '
-                f'{game.field_size}.'
-            )
-            print('Пожалуйста, введите значения для строки и столбца заново.')
-        except ValueError:
-            print('Буквы вводить нельзя. Только числа.')
-            print('Пожалуйста, введите значения для строки и столбца заново.')
-        except Exception as e:
-            print(f'Возникла ошибка: {e}')
 
-            continue
-        else:
-            break
+    while running:
+        print(f'Ход делают {current_player}')
 
 
-    game.make_move(row, column, 'X')
-    print('Ход сделан!')
-    game.display()
+        while True:
+            try:
+                row = int(input('Введите номер строки: '))
+                if row < 0 or row >= game.field_size:
+                    raise FieldIndexError
+                column = int(input('Введите номер столбца: '))
+                if column < 0 or column >= game.field_size:
+                    raise FieldIndexError
+            except FieldIndexError:
+                print(
+                    'Значение должно быть неотрицательным и меньше '
+                    f'{game.field_size}.'
+                )
+                print('Пожалуйста, введите значения для строки и столбца заново.')
+                continue
+            except ValueError:
+                print('Буквы вводить нельзя. Только числа.')
+                print('Пожалуйста, введите значения для строки и столбца заново.')
+                continue
+            except Exception as e:
+                print(f'Возникла ошибка: {e}')
+
+            else:
+                break
+
+
+        game.make_move(row, column, current_player)
+        game.display()
+
+        current_player = '0' if current_player == 'X' else 'X'
 
 if __name__ == '__main__':
     main()
